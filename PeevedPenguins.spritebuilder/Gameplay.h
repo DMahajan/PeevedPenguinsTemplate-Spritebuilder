9606a526f0a8553c26be5a5abb5fe49adebfb402
Sanjay Mahajan <manjirisanjay@Macintosh.local>
2014-08-15 11:42:45 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Sanjay Mahajan on 8/15/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
